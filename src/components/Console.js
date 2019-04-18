1ebbe1f026c4fa65c780c7c253932dcc36b9e1e1
Yizhen Shi <yizhen1001@gmail.com>
2019-04-18 17:44:53 -0400

    add the console

--- src/components/Console.js
+++ src/components/Console.js
@@ -4,24 +4,83 @@ import '../style/console.css';
 
 
 export default class Console extends Component {
+	constructor() {
+		super();
+		this.state = {
+			output:[]
+			
+		}
+}
+
+	addOutput(){
+		var roomNumber = this.props.room.split("");
+		var room = "Room(s) "
+		var safeEnd = " no attack so far " + Date()
+		var attackEnd = "under attack at " + Date()
+		var warningEnd = "near attack room at" + Date()
+		var safe = ""
+		var attack = ""
+		var warning = ""
+		var safeString = ""
+		var warningString =""
+		var attackString=""
+		for(var i=0;i<roomNumber.length;i++){
+			if(roomNumber[i] == 1){
+				attack = attack + i + ", "
+			}else if(roomNumber[i] == 2){
+				warning = warning + i + ", "
+			}else{
+				safe = safe + i+ ", "
+			}
+		}
+		if(safe != ""){
+			safeString = room + safe + safeEnd
+		}
+		if(attack != ""){
+			attackString = room + attack + attackEnd
+		}
+		if(warning != ""){
+			warningString = room + warning + warningEnd;
+			
+		}
+		this.setState({ 
+			output: this.state.output.concat([safeString,attackString,warningString])
+		})
+		
+	}
+
+	
+
+	componentDidMount(){
+		this.setState({room:this.props.room})
+		this.interval = setInterval(() => this.addOutput(), 5000);
+		this.scrollToBottom();
+	}
+
+	componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+		//this.el.scrollTop = this.el.scrollHeight
+    this.el.scrollIntoView({ behavior: 'smooth' });
+  }
 	render(){
+		console.log(this.state)
 		return(
-			<div className="scroller">
-			<div className="console">
-			<p>Section 6 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 2 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 1 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 5 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 4 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 9 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 10 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 6 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 2 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 1 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 5 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 4 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 9 was under attack at 12:12:12 01/01/2019</p>
-			<p>Section 10 was under attack at 12:12:12 01/01/2019</p>
+			<div className="scroller" >
+			<div className="console"  >
+			<div className="output">
+			{
+				this.state.output.map((text)=>{
+					return(
+					<p>{text}</p> 
+						
+					)
+				})
+			}
+			</div>
+			<p ref={el => { this.el = el; }}></p>
 			</div>
 				
 			</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,26 +9,88 @@ import { withRouter } from 'react-router-dom';
 import Map from './Map';
 import About from './About';
 import Console from './Console'
+import data from '../FakeData/data.txt'
+
 
 
 class Home extends Component {
-	getUser(){
-		console.log(this.props.auth)
-		
+	constructor() {
+		super();
+		this.state = {
+			room: {
+				"room1": { "attack": Boolean, "time": "" },
+				"room2": { "attack": Boolean, "time": "" },
+				"room3": { "attack": Boolean, "time": "" },
+				"room4": { "attack": Boolean, "time": "" },
+				"room5": { "attack": Boolean, "time": "" },
+			},
+			console: []
+		}
+
 	}
 
-	componentWillMount(){
-	
+	readTextFile = file => {
+		var rawFile = new XMLHttpRequest();
+		rawFile.open("GET", file, false);
+		rawFile.onreadystatechange = () => {
+			if (rawFile.readyState === 4) {
+				if (rawFile.status === 200 || rawFile.status == 0) {
+					var allText = rawFile.responseText;
+
+					this.setState({
+						rawData: allText
+					});
+				}
+			}
+		};
+		rawFile.send(null);
+	};
+
+	setRoom(){
+		console.log(this.state.rawData)
 	}
-    render() {
-			
-        return (
-						
-            <div>
-                {this.props.auth.user.name?<div><Map/> <Console/></div>:<About/>}
-            </div>
-        );
-    }
+
+
+	getUser() {
+		console.log(this.props.auth)
+
+	}
+	addConsole() {
+		this.setState({ console: this.state.console.concat[Date()] })
+	}
+
+	componentWillMount() {
+		console.log(this.state.console)
+		this.addConsole()
+
+		this.setState({
+			room: {
+				"room1": { "attack": true, "time": "1" },
+				"room2": { "attack": true, "time": "2" },
+				"room3": { "attack": true, "time": "3" },
+				"room4": { "attack": true, "time": "4" },
+				"room5": { "attack": true, "time": "5" },
+			}
+		})
+		this.setState({ console: this.state.console.concat[Date()] })
+	}
+	componentWillMount() {
+		this.readTextFile(data);
+		console.log(this.state)
+	}
+	componentDidMount(){
+		console.log(this.setRoom())
+	}
+	render() {
+		console.log(this.state)
+		
+		return (
+
+			<div>
+				{!this.props.auth.user.name ? <div><Map /> <Console room={this.state.rawData} /></div> : <About />}
+			</div>
+		);
+	}
 }
 const mapStateToProps = (state) => ({
 	auth: state.auth
